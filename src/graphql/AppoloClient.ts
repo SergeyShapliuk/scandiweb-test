f1c4210250cfd7e04cf44f3a47190dfec057e612
SergeyShapliuk <sergeshapluk@gmail.com>
2022-06-08 14:10:52 +0300

    feat: add PUBLIC URL(apolloClient.ts)

--- src/graphql/AppoloClient.ts
+++ src/graphql/AppoloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: 'http://localhost:4000/' || process.env.PUBLIC_URL,
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
