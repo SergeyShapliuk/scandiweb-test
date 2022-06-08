923ff89f8fe2a4307df97c374b3e97437b522561
SergeyShapliuk <sergeshapluk@gmail.com>
2022-06-08 15:04:49 +0300

    fix: update url

--- src/graphql/AppoloClient.ts
+++ src/graphql/AppoloClient.ts
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const url =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000/'
+    : 'https://shop-test-back.herokuapp.com/';
 export const client = new ApolloClient({
-  uri: 'http://localhost:4000/' || process.env.REACT_APP_PUBLIC_URL,
+  uri: url,
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
